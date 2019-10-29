889c6431fc77722bd1e939c664de02ec281d6093
Yinka-femi <yinkafemi@gmail.com>
2019-10-29 16:04:05 +0100

    first commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Todo;
+use Illuminate\Http\Request;
+use App\Http\Resources\TodoResource;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = todo::latest()->get();
+        return new TodoResource($todos);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = request()->validate([
+            'title' => ['required','min:3'],
+            'status'=> ['required']
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        if (is_numeric($id)){
+            $todos = Todo::FindOrFail($id);
+            return new TodoResource($todos);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        if (is_numeric($id)){
+            $todos = Todo::FindOrFail($id);
+            return new TodoResource($todos);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $todos = Todo::findOrFail($id);
+        $updatedTodo = $todos->update(request()->all());
+
+        if ($updatedTodo){
+            return response()->json([
+                'succes' => 'Todo succesfully Updated',
+            ]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $todos = Todo::findOrFail($id);
+        if ($todos){
+            $todos->delete();
+        }
+    }
+}
